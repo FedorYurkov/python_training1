3c49c76e320b92bd9eeba41a68da8ce61e77e0f2
Fedor <fedoryurkov@gmail.com>
2017-12-21 00:42:55 +0300

    Initial commit

---
+++ test_add_group.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from application import Application
+from group import Group
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="group", header="header", footer="gfooter"))
+    app.logout()
+
+
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
